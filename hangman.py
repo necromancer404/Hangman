b27ae0a03031186875d55a3abac6b3a39c1a27bf
necromancer404 <hakeemhamza147@gmail.com>
2023-10-11 23:17:04 +0530

    Started working on the code

--- hangman.py
+++ hangman.py
@@ -2,4 +2,24 @@
 import random
 word_list=["apple","car","banana","laptop"]
 def choose_word():
-return random.choice(word_list)
+	return random.choice(word_list)
+def display_word(word,guessed_letter):
+	display=""
+	for letter in word:
+		if letter in guessed_letters:
+			display+=letter
+		else:
+			display+="_"
+	return display
+def hangman():
+	word=choose_word()
+	guessed_letters= []
+	attempts= 3
+
+
+	print("Hangman")
+	while True:
+		print(display_word(word,guessed_letters))
+		guess=input("Take a guess").lower()
+		
+
